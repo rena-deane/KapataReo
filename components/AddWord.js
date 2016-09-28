25a7f3c6453d5be96d9efe870cfaf6fb9c1485cc
Rena Goldsmith <rena.kg@gmail.com>
2016-09-28 13:32:40 +1300

    remove DatePicker as it throws module error

--- components/AddWord.js
+++ components/AddWord.js
@@ -35,45 +35,54 @@ class AddWord extends Component {
   render() {
     return (
       <View style={styles.addnew}>
-        <Text style={styles.heading}>KUPU MAORI</Text>
-        <TextInput
-          style={styles.newinput}
-          onChangeText={(text) => this.setState({maoriword: text})}
-          placeholder='Maori word'
-        />
 
-        <Text style={styles.heading}>KUPU PAKEHA</Text>
-        <TextInput
-          style={styles.newinput}
-          onChangeText={(text) => this.setState({englishword: text})}
-          placeholder='English word'
-        />
+        <View>
+          <Text style={styles.heading}>KUPU MAORI</Text>
+          <TextInput
+            style={styles.newinput}
+            onChangeText={(text) => this.setState({maoriword: text})}
+            placeholder='Maori word'
+          />
+        </View>
 
-        <Text style={styles.heading}>WHAKAMARAMA</Text>
-        <TextInput
-          style={styles.newinput}
-          multiline = {true}
-          numberOfLines = {4}
-          onChangeText={(text) => this.setState({description: text})}
-          placeholder='Add sentences using the word or description'
-        />
+        <View>
+          <Text style={styles.heading}>KUPU PAKEHA</Text>
+          <TextInput
+            style={styles.newinput}
+            onChangeText={(text) => this.setState({englishword: text})}
+            placeholder='English word'
+          />
+        </View>
 
-        <Text style={styles.heading}>MOMO</Text>
-        <PickerIOS
-          itemStyle={{fontSize: 24, textAlign: 'left'}}
-          selectedValue={this.state.type}
-          onValueChange={(newtype) => this.setState({type: newtype})}>
-          {TYPES.map((type, index) => (
-            <PickerItemIOS
-              key={index}
-              value={type}
-              label={type}
-            />
-          ))}
-        </PickerIOS>
+        <View>
+          <Text style={styles.heading}>WHAKAMARAMA</Text>
+          <TextInput
+            style={styles.newinput}
+            multiline = {true}
+            numberOfLines = {4}
+            onChangeText={(text) => this.setState({description: text})}
+            placeholder='Add sentences using the word or description'
+          />
+        </View>
+
+        <View>
+          <Text style={styles.heading}>MOMO</Text>
+          <PickerIOS
+            itemStyle={styles.picker}
+            selectedValue={this.state.type}
+            onValueChange={(newtype) => this.setState({type: newtype})}>
+            {TYPES.map((type, index) => (
+              <PickerItemIOS
+                key={index}
+                value={type}
+                label={type}
+              />
+            ))}
+          </PickerIOS>
+        </View>
 
         <TouchableHighlight onPress={this.props.submit} style={styles.submit}>
-          <Text style={styles.actionText}>Add Word</Text>
+          <Text style={styles.submitText}>tapiri</Text>
         </TouchableHighlight>
       </View>
     );
